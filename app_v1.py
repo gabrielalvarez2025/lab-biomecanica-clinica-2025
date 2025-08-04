cf164a41d0e76c068ecf00979dccad0dab11af4a
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-03 22:04:22 -0400

    Actualización del script

--- app_v1.py
+++ app_v1.py
@@ -12,49 +12,55 @@ st.title("Espacio Interactivo LabBC")
 st.subheader("**Análisis Biomecánico del Movimiento**")
 
 st.markdown("Estimado/a estudiante. "
-            "Bienvenido/a a este espacio interactivo del curso de Análisis Biomecánico del Movimiento." \
+            "Bienvenido/a a este espacio interactivo del curso de Análisis Biomecánico del Movimiento. " \
             "Aquí encontrarás recursos y herramientas interactivas para complementar tu aprendizaje."
-            "---")
+            )
+st.markdown("---")
 
 st.info("⬅ Usa el menú lateral para navegar por las unidades cuando estén disponibles.")
 
-
+pag_bienvenida = "Bienvenida"
+pag_introduccion = "Sección 1: Introducción"
+pag_bioinstrumentacion = "Sección 2: Bioinstrumentación"
+pag_control_motor = "Sección 3: Teorías del control motor"
+pag_balance = "Sección 4: Sistema sensoriomotor y balance"
+pag_marcha = "Sección 5: Análisis de marcha"
 
 
 # Sidebar con selector de página
 pagina = st.sidebar.radio("Selecciona una sección:", [
-    "Bienvenida",
-    "Unidad 1: Introducción",
-    "Unidad 2: Bioinstrumentación",
-    "Unidad 3: Teorías del control motor",
-    "Unidad 4: Sistema sensoriomotor y balance",
-    "Unidad 5: Análisis de marcha"
+    pag_bienvenida,
+    pag_introduccion,
+    pag_bioinstrumentacion,
+    pag_control_motor,
+    pag_balance,
+    pag_marcha
 ])
 
 # Mostrar contenido según la selección
-if pagina == "Bienvenida":
+if pagina == pag_bienvenida:
     st.title("Te damos la Bienvenida 👋")
     st.write("Usa el menú de la izquierda para navegar.")
 
 
 
 
-elif pagina == "Unidad 1: Introducción":
+elif pagina == pag_introduccion:
     st.header("📘 Unidad 1: Introducción al análisis del movimiento")
     st.markdown("- Etapas del análisis\n- Cinemática\n- Cinética")
 
-elif pagina == "Unidad 2: Bioinstrumentación":
+elif pagina == pag_bioinstrumentacion:
     st.header("🧪 Unidad 2: Bioinstrumentación")
     st.markdown("- Electromiografía (EMG)\n- Plataforma de fuerza\n- Videofotogrametría\n- Goniometría")
 
-elif pagina == "Unidad 3: Teorías del control motor":
+elif pagina == pag_control_motor:
     st.header("🧠 Unidad 3: Teorías del control motor")
     st.markdown("- Teorías antiguas\n- Teoría de sistemas dinámicos\n- Teoría de modelos internos")
 
-elif pagina == "Unidad 4: Sistema sensoriomotor y balance":
+elif pagina == pag_balance:
     st.header("⚖️ Unidad 4: Sistema sensoriomotor y balance")
 
-elif pagina == "Unidad 5: Análisis de marcha":
+elif pagina == pag_marcha:
     st.header("🚶 Unidad 5: Análisis de marcha")
 
 # Mensaje de bienvenida
