badacc6927caf03bac05607d18f7b2e0bc46caf0
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-04 00:24:36 -0400

    Actualizar

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -35,6 +35,8 @@ def mostrar():
                 
     """)
 
+    # ---------------------- Configuración de sidebar del elemento interactivo ----------------------
+
     # Parámetros de la simulación, sidebar
     st.sidebar.title("Parámetros de simulación")
     num_ondas = st.sidebar.slider("Número de ondas", 1, 15, 6)
@@ -48,8 +50,11 @@ def mostrar():
     suma_total = np.zeros_like(x)
     colores_pastel = sns.color_palette("pastel", num_ondas)
 
+    
+    # ---------------------- Sliders de cada onda (generarlos) ----------------------
+
     # Sliders por onda
-   # Inicializar session_state para cada parámetro si no existe o si cambió num_ondas
+    # Inicializar session_state para cada parámetro si no existe o si cambió num_ondas
     for i in range(num_ondas):
         if f"amp_{i}" not in st.session_state or st.session_state.get("num_ondas_actual", None) != num_ondas:
             st.session_state[f"amp_{i}"] = 1.0
@@ -60,7 +65,13 @@ def mostrar():
     
     st.session_state["num_ondas_actual"] = num_ondas
 
-    # Botones en fila
+    
+    # ---------------------- Botones de reinicio y aleatorio ----------------------
+    
+    # Instrucciones botones random y reiniciar:
+    
+    
+    # Configurar botones random y reiniciar en la sidebar
     esp1, col1, col2, esp2 = st.sidebar.columns([1, 2, 2, 1])  # proporciones
 
 
@@ -78,6 +89,8 @@ def mostrar():
                 st.session_state[f"freq_{i}"] = random.uniform(0, freq_max)
                 st.session_state[f"fase_{i}"] = random.uniform(0, fase_max)
 
+    # ---------------------- Sliders de cada onda (lectura) ----------------------
+    
     # Leer sliders con los valores del session_state
     params = []
     for i in range(num_ondas):
@@ -113,6 +126,8 @@ def mostrar():
 
     st.pyplot(fig1)
 
+    # ---------------------- Texto pre - Onda Sumatoria ----------------------
+
     st.markdown("""
                 Las ondas individuales representan las unidades motoras (UM) que se suman para formar una señal EMG compuesta. Cuando las {n_ondas} ondas ocurren simultáneamente (chocan, se combinan), interfieren entre sí constructivamente en algunas zonas y destructivamente en otras.
                 El resultado de sumar gráficamente esas {n_ondas} ondas que configuraste se muestra en la siguiente **onda resultante**:
@@ -129,7 +144,7 @@ def mostrar():
     ax2.set_ylim(-amp_max * num_ondas, amp_max * num_ondas)
     ax2.set_ylabel("Suma", color='white', alpha=1)
     ax2.set_xlabel("Tiempo (ms)", color='white')
-    ax2.set_title("Simulación de señal EMG compuesta", color='white')
+    ax2.set_title("Simulación de la onda resultante de la sumatoria", color='white')
 
     ax2.grid(True, alpha=0.10)
     ax2.tick_params(axis='x', colors='white')
