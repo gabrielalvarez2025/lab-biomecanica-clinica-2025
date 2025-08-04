ea66122ec460db67914221972559ed3cf579769a
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-03 22:55:50 -0400

    Subir nuevas secciones al repositorio

--- pag_bienvenida.py
+++ pag_bienvenida.py
@@ -3,3 +3,32 @@ import streamlit as st
 def mostrar():
     st.title("Te damos la Bienvenida 👋")
     st.write("Usa el menú de la izquierda para navegar.")
+
+    # Unidades del curso
+    st.markdown("""
+    A continuación, te presentamos una visión general de las **unidades** que abordaremos:            
+    
+    ### Unidades del curso
+
+    1. **Introducción al análisis del movimiento**
+    - Etapas del análisis
+    - Cinemática
+    - Cinética
+
+    2. **Bioinstrumentación**
+    - Electromiografía (EMG)
+    - Plataforma de fuerza
+    - Videofotogrametría
+    - Goniometría
+
+    3. **Teorías del control motor**
+    - Teorías antiguas
+    - Teoría de sistemas dinámicos
+    - Teoría de modelos internos
+
+    4. **Sistema sensoriomotor y balance**
+
+    5. **Análisis de marcha**
+    """)
+
+    st.markdown("---")
