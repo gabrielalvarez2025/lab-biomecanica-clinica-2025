0bb686c2327ada81973b6f282e575e267a7d0626
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-05 18:01:28 -0400

    Actualizar todo

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -224,30 +224,22 @@ def botones_tarjeta():
 
     
     with col1:
-        with st.container():
-            st.markdown("""
-                <div style='
-                    background-color: #f9f9f9;
-                    padding: 20px;
-                    border-radius: 10px;
-                    border: 1px solid #ddd;
-                    box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
-                '>
+        if st.button("Sumatoria de ondas"):
+            st.session_state["mostrar_sumatoria"] = True
+            st.info("Estas viendo esta simulación.")
+
+        st.markdown("""
+            <div style="
+                background-color: #f5f5f5;
+                border: 1px solid #d3d3d3;
+                padding: 16px;
+                border-radius: 8px;
+                color: #444;
+                font-size: 16px;
+                margin-top: 10px;">
+                Si tienes dudas de por qué la señal de EMG tiene la forma que tiene o cuál es su relación con los potenciales de acción de unidades motoras (PAUMs), esta simulación te ayudará a entenderlo.
+            </div>
             """, unsafe_allow_html=True)
-
-            # Botón
-            if st.button("Sumatoria de ondas"):
-                st.session_state["mostrar_sumatoria"] = True
-                st.info("Estas viendo esta simulación.")
-
-            # Texto
-            st.markdown("""
-                <div style='color: gray; font-size: 16px;'>
-                    Si tienes dudas de por qué la señal de EMG tiene la forma que tiene o cuál es su relación con los potenciales de acción de unidades motoras (PAUMs), esta simulación te ayudará a entenderlo.
-                </div>
-            """, unsafe_allow_html=True)
-
-            st.markdown("</div>", unsafe_allow_html=True)
         
 
     with col2:
