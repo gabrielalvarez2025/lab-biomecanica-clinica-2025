6892b4762fbc2cd557f51c5b9573694ea2157664
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-05 01:20:24 -0400

    Actualizar todo

--- pag_marcha.py
+++ pag_marcha.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pyvista as pv
-from stpyvista import stpyvista
+
 
 def mostrar():
     
@@ -8,9 +7,5 @@ def mostrar():
     st.header("Unidad 5: Análisis de marcha")
 
     # Cargar y mostrar modelo 3D
-    plotter = pv.Plotter()
-    plotter.add_mesh(pv.read("modelo.glb"))  # usa .glb o .gltf convertido desde .fbx
-
-    stpyvista(plotter)
-
+    
     
