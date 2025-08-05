7de7596635e40c763424997d11d267ae14873773
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-05 17:59:11 -0400

    Actualizar todo

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -225,16 +225,13 @@ def botones_tarjeta():
     
     with col1:
         
-        st.info(
-            if st.button("Sumatoria de ondas"):
+        if st.button("Sumatoria de ondas"):
             st.session_state["mostrar_sumatoria"] = True
             st.info("Estas viendo esta simulación.")
         
-            st.markdown("""<div style='color: gray; font-size: 16px;'>
-                        Si tienes dudas de por qué la señal de EMG tiene la forma que tiene o cuál es su relación con los potenciales de acción de unidades motoras (PAUMs), esta simulación te ayudará a entenderlo.
-                        </div>""", unsafe_allow_html=True)
-        )
-        
+        st.markdown("""<div style='color: gray; font-size: 16px;'>
+                    Si tienes dudas de por qué la señal de EMG tiene la forma que tiene o cuál es su relación con los potenciales de acción de unidades motoras (PAUMs), esta simulación te ayudará a entenderlo.
+                    </div>""", unsafe_allow_html=True)
         
 
     with col2:
