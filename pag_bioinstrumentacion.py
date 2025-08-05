8406a0a55cdd181fa428e399a61ab93e75af9c73
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-05 19:38:10 -0400

    Actualizar todo

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -241,7 +241,7 @@ def botones_tarjeta():
 
     with col1:
         # Este bloque añade la clase personalizada al botón
-        st.markdown(" ")
+        #st.markdown(" ")
         st.markdown('<div class="stButton"><button class="custom-boton">Sumatoria de ondas</button></div>', unsafe_allow_html=True)
 
         # Si quieres que este botón active la función en Python, usa st.button y el estilo afectará a todos los botones.
