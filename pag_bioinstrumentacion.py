6daf08e99b3b14c72f9f4c347dd50484c53c3971
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-05 14:34:18 -0400

    Actualizar todo

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -218,12 +218,20 @@ def botones_tarjeta():
     col1, col2 = st.columns(2)
 
     with col1:
-        if st.button("Sumatoria de ondas"):
-            st.session_state["mostrar_sumatoria"] = True
-            st.info("Estas viendo esta simulación.")
+        st.markdown(" ")
+        st.markdown("""
+                    1. **Simulación de la sumatoria de ondas**:" \
+                    "Si tienes dudas de por qué la forma de la señal de EMG es tan irregular," \
+                    "esta simulación te ayudará a entenderlo." \
+                    """)
+        
 
     with col2:
-        st.markdown("Simulación de la sumatoria de ondas:")
+        
+        if st.button("Sumatoria de ondas"):
+            st.markdown("**Simulaciones:**")
+            st.session_state["mostrar_sumatoria"] = True
+            st.info("Estas viendo esta simulación.")
         
         
 
