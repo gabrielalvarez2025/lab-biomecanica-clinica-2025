189f0949c58189b84fcfc5cc9326ae44f6ec0ce8
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-05 19:37:52 -0400

    Actualizar todo

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -248,14 +248,14 @@ def botones_tarjeta():
         # O bien, con JavaScript puedes detectar clics en el botón HTML (más avanzado).
 
         st.markdown("""
-            <p style="color: #666; font-size: 16px; margin-top: 0px;">
+            <p style="color: #666; font-size: 16px; margin-top: 10px;">
             Si tienes dudas de por qué la señal de EMG tiene la forma que tiene o cuál es su relación con los potenciales de acción de unidades motoras (PAUMs), esta simulación te ayudará a entenderlo.
             </p>
         """, unsafe_allow_html=True)
 
     with col2:
         st.markdown("""
-            <p style="color: #666; font-size: 16px; margin-top: 10px;">
+            <p style="color: #666; font-size: 16px; margin-top: 0px;">
             Si tienes dudas de por qué la señal de EMG tiene la forma que tiene o cuál es su relación con los potenciales de acción de unidades motoras (PAUMs), esta simulación te ayudará a entenderlo.
             </p>
         """, unsafe_allow_html=True)
