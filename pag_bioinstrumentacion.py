2281c2a8b97eb510a032a73a178c56c85e6da81b
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-05 21:33:28 -0400

    Actualizar todo

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -281,7 +281,7 @@ def botones_tarjeta():
 
     with col2:
         st.markdown("""
-            <p style="font-size: 16px; margin-top: 10px; justify-content: center; text-align: justify;">
+            <p style="font-size: 16px; margin-top: 10px;">
             Si tienes dudas de por qué la señal de EMG tiene la forma que tiene o cuál es su relación con los potenciales de acción de unidades motoras (PAUMs), esta simulación te ayudará a entenderlo.
             </p>
         """, unsafe_allow_html=True)
