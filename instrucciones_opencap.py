286cf2d9006be8b261aae022a84cf4c49e0bae3e
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-07 16:28:23 -0400

    Actualizar todo

--- instrucciones_opencap.py
+++ instrucciones_opencap.py
@@ -65,8 +65,8 @@ def instrucciones():
     
     # Fila laptop
     with col1_laptop:
-        st.image(img_laptop, use_container_width=True)
         st.markdown(" ")
+        st.image(img_laptop, use_container_width=True)
         st.markdown(" ")
     
     with col2_laptop:
