df41ad75642ca442b98f15dd0b3a6d3054413fc0
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-07 16:10:18 -0400

    Actualizar todo

--- instrucciones_opencap.py
+++ instrucciones_opencap.py
@@ -27,13 +27,42 @@ def instrucciones():
 
     st.markdown(
         """
-        Para utilizar OpenCap, asegúrate de tener lo siguiente:
+        Para utilizar OpenCap, primero debes:
+
         - Crear una cuenta en OpenCap. Puedes crear una cuenta gratuita en https://www.opencap.ai/ con tu correo electrónico.
         - Contar con **dos o más** dispositivos iOS (iPhone, iPad, etc) con cámara. Estos deben ser iOS (marca Apple).
         - Descargar la aplicación de OpenCap en ambos dispositivos iOS desde la App Store.
         - Disponer de un espacio bien iluminado y más bien amplio para realizar las grabaciones.
         """
     )
+
+    proporcion = [15, 85]  # Proporción de columnas
+    
+    # imagenes
+    img_laptop = open("img_laptop.svg", "r").read()
+    img_cellphones2 = open("img_cellphones2.svg", "r").read()
+    
+    
+    col1_laptop,    col2_laptop     = st.columns(proporcion)
+    col1_cellphone, col2_cellphone  = st.columns(proporcion)
+    
+    with col1_laptop:
+        st.image(img_laptop, use_container_width=True)
+        st.markdown(" ")
+        st.markdown(" ")
+    
+    with col2_laptop:
+        st.markdown("""[] Un computador. Eventualmente puede ser un celular o una tablet, pero debe ser distinto a los dispositivos que usarás como cámaras.""")
+    
+    ###
+
+    with col1_cellphone:
+        st.image(img_cellphones2, use_container_width=True)
+    with col2_cellphone:
+        st.markdown("""- Al menos 2 dispositivos iOS (iPhone, iPad, etc) con cámara, que usarás para registrar los videos. Estos necesariamente **deben** ser iOS (marca Apple).""")
+
+    
+
 
     st.markdown("---")
 
@@ -51,41 +80,7 @@ def instrucciones():
         """
     )
 
-    st.markdown(
-        """
-        Luego, debes asegurart de que:
-        - Los +2 dispositivos iOS estén conectados a la misma red Wi-Fi.
-        """
-    )
-
-    proporcion = [15, 85]  # Proporción de columnas
     
-    # imagenes
-    img_laptop = open("img_laptop.svg", "r").read()
-    img_cellphones2 = open("img_cellphones2.svg", "r").read()
-    
-
-
-    col1_laptop, col2_laptop = st.columns(proporcion)
-    col1_cellphone, col2_cellphone = st.columns(proporcion)
-    
-    with col1_laptop:
-        st.image(img_laptop, use_container_width=True)
-        st.markdown(" ")
-        st.markdown(" ")
-    
-    with col2_laptop:
-        st.markdown("""- Un computador. Eventualmente puede ser un celular o una tablet, pero debe ser distinto a los dispositivos que usarás como cámaras.""")
-    
-    ###
-
-    with col1_cellphone:
-        st.image(img_cellphones2, use_container_width=True)
-    with col2_cellphone:
-        st.markdown("""- Al menos 2 dispositivos iOS (iPhone, iPad, etc) con cámara, que usarás para registrar los videos. Estos necesariamente **deben** ser iOS (marca Apple).""")
-
-    
-
     
 
 
