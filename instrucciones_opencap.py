0f5765141dc8cc1da83e3251e48bfbc9f1fde2c8
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-07 15:58:31 -0400

    Actualizar todo

--- instrucciones_opencap.py
+++ instrucciones_opencap.py
@@ -71,7 +71,8 @@ def instrucciones():
     
     with col1_laptop:
         st.image(img_laptop, use_container_width=True)
-        st.markdown(" ")
+        st.empty()
+        st.empty()
     
     with col2_laptop:
         st.markdown("""- Un computador. Eventualmente puede ser un celular o una tablet, pero debe ser distinto a los dispositivos que usarás como cámaras.""")
