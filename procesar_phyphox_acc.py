a556a55b2600bff5111d67917b80736c77232f90
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-09 17:27:28 -0400

    Actualizar todo

--- procesar_phyphox_acc.py
+++ procesar_phyphox_acc.py
@@ -67,10 +67,10 @@ def main_phyphox():
 
         # Fondo transparente también para la leyenda
         legend = ax.legend()
-        legend.get_frame().set_facecolor("none")
+        legend.get_frame().set_facecolor("white")
         legend.get_frame().set_edgecolor("white")
         for text in legend.get_texts():
-            text.set_color("white")
+            text.set_color("k")
 
         # Mostrar gráfico en Streamlit
         st.pyplot(fig, transparent=True)
