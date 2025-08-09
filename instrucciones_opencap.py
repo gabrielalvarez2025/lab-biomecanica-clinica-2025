312270970098d5b310121122ac7473ab6adaa994
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-08 21:40:41 -0400

    Actualizar todo

--- instrucciones_opencap.py
+++ instrucciones_opencap.py
@@ -120,7 +120,7 @@ def main_instrucciones_opencap():
 
     st.markdown("---")
 
-    st.subheader("Preparando el espacio de trabajo", divider="white")
+    st.subheader("Preparando el espacio de trabajo", divider="gray")
     
     col_espacio1, col_espacio2 = st.columns([60, 40])
 
