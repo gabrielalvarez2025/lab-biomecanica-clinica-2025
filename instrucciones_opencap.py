ad7c7dc3b876105b38d398803017f0d76bdbf40f
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-08 23:35:33 -0400

    Actualizar todo

--- instrucciones_opencap.py
+++ instrucciones_opencap.py
@@ -134,7 +134,7 @@ def main_instrucciones_opencap():
             Asegúrate de que el espacio donde <u>realizarás</u> las grabaciones esté **bien iluminado** 💡 y libre de obstáculos. 
             
             Es importante que el área sea lo suficientemente amplia para que la persona pueda moverse con libertad y que las cámaras puedan capturar todo el movimiento sin obstrucciones.
-            """
+            """, unsafe_allow_html=True
         )
         st.warning(
             """
