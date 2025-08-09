47e5c78a37962c55148fb5453d58626ccbdacb9c
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-08 22:34:41 -0400

    Actualizar todo

--- instrucciones_opencap.py
+++ instrucciones_opencap.py
@@ -134,7 +134,7 @@ def main_instrucciones_opencap():
             """
         )
 
-        st.markdown("##### Cámaras:")
+        st.markdown("##### Cámaras:", font_color="blue")
         st.info(
             """
             Coloca tus cámaras (iPhones, iPads, etc) sobre trípodes o superficies fijas alrededor del área de grabación. Usaremos la cámara trasera de tus dispositivos.
