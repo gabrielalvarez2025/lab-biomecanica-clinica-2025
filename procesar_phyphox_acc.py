6fd5f6f5373a1d7dcd2513b97641c0dc62d070f0
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-10 03:23:33 -0400

    Actualizar todo

--- procesar_phyphox_acc.py
+++ procesar_phyphox_acc.py
@@ -33,7 +33,7 @@ def main_phyphox():
             show_z = st.checkbox("Acc Z (amarillo)", value=True)
             show_abs = st.checkbox("Absolute (blanco)", value=True)
         with col2:
-            start_time = st.number_input("Mostrar **desde** el segundo:", min_value=min_time, max_value=max_time, value=min_time, step=0.1)
+            start_time = st.number_input("Mostrar <u>**desde**</u> el segundo:", min_value=min_time, max_value=max_time, value=min_time, step=0.1)
             end_time = st.number_input("Tiempo final (s)", min_value=min_time, max_value=max_time, value=max_time, step=0.1)
 
         # Filtrar datos
