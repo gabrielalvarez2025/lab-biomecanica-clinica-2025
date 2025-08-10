38bd822ce4002e1034017633f21be9c06d088623
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-10 05:30:44 -0400

    Actualizar todo

--- procesar_phyphox_acc.py
+++ procesar_phyphox_acc.py
@@ -73,8 +73,12 @@ def main_phyphox():
 
             st.markdown("###### Puedes ajustar la ventana de tiempo que te interesa mirar:")
             
-            start_time = st.number_input("Mostrar tiempo **desde** el segundo:", min_value=min_time, max_value=max_time, value=min_time, step=0.1)
-            end_time = st.number_input("Mostrar tiempo **hasta** el segundo:", min_value=min_time, max_value=max_time, value=max_time, step=0.1)
+            col_tiempo1, col_tiempo2 = st.columns(2)
+
+            with col_tiempo1:
+                start_time = st.number_input("Mostrar tiempo **desde** el segundo:", min_value=min_time, max_value=max_time, value=min_time, step=0.1)
+            with col_tiempo2:
+                end_time = st.number_input("Mostrar tiempo **hasta** el segundo:", min_value=min_time, max_value=max_time, value=max_time, step=0.1)
 
 
         st.markdown(" ")
