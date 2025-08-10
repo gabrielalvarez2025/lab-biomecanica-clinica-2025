78b2f0c7d2b473e6d3adeb96e574fed90fa11fee
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-10 06:36:41 -0400

    Actualizar todo

--- procesar_amti.py
+++ procesar_amti.py
@@ -34,10 +34,8 @@ def main_forceplate():
 
         col_frame1, col_frame2 = st.columns(2)
 
-        with col_frame1:
-            start_frame = st.number_input("Desde Frame:", min_value=min_frame, max_value=max_frame, value=min_frame, step=1)
-        with col_frame2:
-            end_frame = st.number_input("Hasta Frame:", min_value=min_frame, max_value=max_frame, value=max_frame, step=1)
+        start_frame = st.number_input("Desde Frame:", min_value=min_frame, max_value=max_frame, value=min_frame, step=1)
+        end_frame = st.number_input("Hasta Frame:", min_value=min_frame, max_value=max_frame, value=max_frame, step=1)
 
         df_filtered = df[(df["Frame"] >= start_frame) & (df["Frame"] <= end_frame)]
 
