3b6ec35b8b172f96c66878dba0e7a10b1595c187
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-10 05:13:09 -0400

    Actualizar todo

--- procesar_phyphox_acc.py
+++ procesar_phyphox_acc.py
@@ -122,17 +122,17 @@ def main_phyphox():
 
                 #st.markdown(f"### Ajuste de eje Y para {label}")
                 st.markdown(f"Ajusta el eje Y:")
-                y_min = st.number_input(
-                    f"min:",
-                    value=float(df_filtered[col].min()),
-                    step=0.5,
-                    key=f"ymin_{col}"
-                )
                 y_max = st.number_input(
                     f"max:",
                     value=float(df_filtered[col].max()),
                     step=0.5,
                     key=f"ymax_{col}"
+                )
+                y_min = st.number_input(
+                    f"min:",
+                    value=float(df_filtered[col].min()),
+                    step=0.5,
+                    key=f"ymin_{col}"
                 )
             
             with col_plot1:
