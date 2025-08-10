e59c2126bb172b268db31dacf36938d549fba11e
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-10 07:31:01 -0400

    Actualizar todo

--- procesar_amti.py
+++ procesar_amti.py
@@ -65,7 +65,7 @@ def main_forceplate():
         df = df[cols]
 
         st.write("Veamos cómo se ve tu tabla con tiempo:")
-        st.write(" 👇")
+        st.markdown("####### 👇🏼")
         st.dataframe(df, hide_index=True)
 
         
