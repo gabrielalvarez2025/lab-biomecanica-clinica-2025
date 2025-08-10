403aa335dc5849b984f5c074a810cd4dece5b284
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-10 07:28:10 -0400

    Actualizar todo

--- procesar_amti.py
+++ procesar_amti.py
@@ -78,8 +78,8 @@ def main_forceplate():
 
         st.markdown("#### Ajusta ventana de frames para graficar:")
 
-        start_time = st.number_input("Desde seg:", min_value=min_frame, max_value=max_frame, value=min_frame, step=1)
-        end_time = st.number_input("Hasta seg:", min_value=min_frame, max_value=max_frame, value=max_frame, step=1)
+        start_time = st.number_input("Desde seg:", min_value=min_frame, max_value=max_frame, value=min_frame, step=1.0)
+        end_time = st.number_input("Hasta seg:", min_value=min_frame, max_value=max_frame, value=max_frame, step=1.0)
 
         df_filtered = df[(df["Tiempo"] >= start_frame) & (df["Tiempo"] <= end_frame)]
 
