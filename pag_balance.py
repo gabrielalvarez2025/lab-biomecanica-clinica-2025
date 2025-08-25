3250c5c6cf9421bed2f5d73f5747aefe1be386de
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-25 02:09:44 -0400

    Actualizar todo

--- pag_balance.py
+++ pag_balance.py
@@ -116,7 +116,7 @@ def main_balance():
     col1, col2 = st.columns(2)
     oculto = st.session_state.oculto
 
-    symbol_incognita = "❑"
+    symbol_incognita = "◼"
 
     with col1:
         st.markdown(f"## 📝 Desafío:")
