2aeeecd5b758b10b972981dd5e508f720247a00c
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-27 10:17:19 -0400

    Actualizar todo

--- procesar_opencap.py
+++ procesar_opencap.py
@@ -239,7 +239,7 @@ def main_opencap():
                     col_plot_ang_1, col_plot_ang_2 = st.columns([1, 3])
 
                     with col_plot_ang_1:
-                        uploaded_video = render_video(z, video_paths, cam_map, label="Ángulo–Ángulo")
+                        uploaded_video = render_video(z, video_paths, cam_map, label="Ángulo vs Tiempo")
 
                     with col_plot_ang_2:
                     
