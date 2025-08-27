383edefb1735b3601e3fa8f670d5f4eb0e3d6980
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-08-26 22:39:35 -0400

    Actualizar todo

--- procesar_opencap.py
+++ procesar_opencap.py
@@ -229,14 +229,7 @@ def main_opencap():
 
                     if uploaded_video is not None:
                         with col_plot_ang_1:
-                            st.markdown(" ")
-                            st.markdown(" ")
-                            st.markdown(" ")
-                            st.markdown(" ")
-                            st.markdown(" ")
-                            st.markdown(" ")
-                            # flechas cambiar Cam0 - Cam1 - Cam2
-                            st.video(uploaded_video, loop=True, muted=True)
+                            video_lateral()
 
                     with col_plot_ang_2:
                     
