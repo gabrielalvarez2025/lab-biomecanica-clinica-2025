9f59c70234f7f0939c0307a4e23c0af3bc291872
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-15 13:04:14 -0300

    Actualizar todo

--- procesar_phyphox_acc.py
+++ procesar_phyphox_acc.py
@@ -213,9 +213,10 @@ def ejemplo_fr_botas():
         st.markdown(" ")
         st.markdown(" ")
         st.markdown(" ")
-        st.markdown(" ")
-        st.image("cat1.jpg", use_container_width=True)
+        
         st.markdown("Este es Botas. Con el objetivo de medir su frecuencia respiratoria en reposo de forma más exacta, colocamos un celular en su lomo mientras dormía.")
+        st.image("cat1.jpg", use_container_width=True)
+        st.markdown("Con el objetivo de medir su frecuencia respiratoria en reposo de forma más exacta, colocamos un celular en su lomo mientras dormía.")
 
     with col_text:
         st.markdown(" ")
