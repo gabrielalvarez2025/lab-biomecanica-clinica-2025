5b19787991caf4b27a984bbc593b1fcb8401b3ee
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-20 20:11:24 -0300

    Actualizar todo

--- procesar_phyphox_acc.py
+++ procesar_phyphox_acc.py
@@ -305,7 +305,7 @@ def ejemplo_fr_botas():
         # -----------------------
         # Inputs interactivos para filtro
         # -----------------------
-        st.markdown("##### Ajusta el filtro Butterworth")
+        st.markdown("##### Ajusta aquí el filtro digital")
         low_cut, high_cut = st.slider(
             "Elige la banda de frecuencias (Hz) que dejarás pasar:",
             min_value=0.0,
