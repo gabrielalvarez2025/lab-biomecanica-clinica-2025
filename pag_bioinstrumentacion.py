2efc0c3020ff617151ea7755f86c4ea6995700a0
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-21 13:21:37 -0300

    Actualizar todo

--- pag_bioinstrumentacion.py
+++ pag_bioinstrumentacion.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
 import random
 import streamlit.components.v1 as components
 
