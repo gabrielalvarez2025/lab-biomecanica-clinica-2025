385986a1b9bce04f395479a438710d0959844960
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-21 23:58:39 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -359,7 +359,7 @@ def main_control_motor():
     st.markdown("En la hipótesis UCM, las variaciones en los grados de libertad se dividen en dos componentes: ")
 
 
-    esp1, col_plot, esp2 = st.columns([1, 99, 1])
+    esp1, col_plot, esp2 = st.columns([25, 50, 25])
 
     with col_plot:
         st.plotly_chart(crear_plot_sinergia_ucm(title="Sinergia", synergy=True, ratio_var=1, mostrar_numeros=True, boton_agregar=True), 
