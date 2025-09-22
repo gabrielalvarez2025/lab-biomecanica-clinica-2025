e18780b9acf3235b19b167b49277efd433d61b2a
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-22 02:33:34 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -408,7 +408,7 @@ def main_control_motor():
         
         # SÍ es sinergia (ratio > 1)
         plot_es_sinergia = crear_plot_sinergia_ucm(mostrar_numeros=True,
-                                                   var_ucm=0.7,
+                                                   var_ucm=0.8,
                                                    var_ort=0.2
                                                    )
         
