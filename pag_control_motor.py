30e40bdfbc8908f52913b3486f4a0047d52ec657
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-21 22:24:31 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -277,7 +277,7 @@ def main_control_motor():
 
         # Layout
         fig.update_layout(
-            title=dict(text=f"{subtitle}", x=0.1),
+            title=dict(text=f"{subtitle}", x="center"),
             xaxis=dict(
                 range=[0, 12],
                 showgrid=False,
