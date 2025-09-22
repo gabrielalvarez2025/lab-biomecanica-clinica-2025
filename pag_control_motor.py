b2470e5b8f2038502ab53945e4225a635530a041
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-22 03:09:40 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -437,4 +437,25 @@ def main_control_motor():
                                                    )
         
         st.plotly_chart(plot_es_sinergia, use_container_width=True, config={"staticPlot": True})
+
+
+    col_sinergia_debil, esp_sinergias, col_sinergia_fuerte = st.columns([0.49, 0.02, 0.49])
+
+    with col_sinergia_debil:
+        
+        plot_sinergia_debil = crear_plot_sinergia_ucm(mostrar_numeros=False, mostrar_elipse=1,
+                                                   var_ucm=0.8,
+                                                   var_ort=0.2
+                                                   )
+        
+        st.plotly_chart(plot_sinergia_debil, use_container_width=True, config={"staticPlot": True})
+
+    with col_sinergia_fuerte:
+        
+        plot_sinergia_fuerte = crear_plot_sinergia_ucm(mostrar_numeros=False, mostrar_elipse=1,
+                                                   var_ucm=0.8,
+                                                   var_ort=0.2
+                                                   )
+        
+        st.plotly_chart(plot_sinergia_fuerte, use_container_width=True, config={"staticPlot": True})
     
