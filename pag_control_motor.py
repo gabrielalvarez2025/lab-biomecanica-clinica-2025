4af915760b604a78d0ae049d09c4dceac3a41dfd
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-22 03:06:51 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -433,7 +433,7 @@ def main_control_motor():
         # SÍ es sinergia (ratio > 1)
         plot_es_sinergia = crear_plot_sinergia_ucm(mostrar_numeros=False, mostrar_elipse=False,
                                                    var_ucm=0.8,
-                                                   var_ort=0.3
+                                                   var_ort=0.1
                                                    )
         
         st.plotly_chart(plot_es_sinergia, use_container_width=True, config={"staticPlot": True})
