80045d1face9b5295de3786f27036b6e851a981e
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-22 03:03:11 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -412,7 +412,7 @@ def main_control_motor():
     with col1:
         
         # NO es sinergia (ratio <= 1)
-        plot_no_sinergia = crear_plot_sinergia_ucm(mostrar_numeros=False, dof_y="Ángulo de rodilla", dof_x="Ángulo de cadera",
+        plot_no_sinergia = crear_plot_sinergia_ucm(mostrar_numeros=False,
                                                    var_ucm=0.3,
                                                    var_ort=0.45
                                                    )
