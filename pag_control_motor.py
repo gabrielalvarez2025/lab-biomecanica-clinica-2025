894d6af19cef66dbe8e68094098d1b611e529247
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-22 00:27:07 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -380,7 +380,7 @@ def main_control_motor():
     col1, esp, col2 = st.columns([0.49, 0.02, 0.49])
     
     with col1:
-        st.plotly_chart(crear_plot_sinergia_ucm(title="Not a synergy", synergy=False, ratio_var=40, mostrar_numeros=True, mostrar_elipse=False), 
+        st.plotly_chart(crear_plot_sinergia_ucm(title="Not a synergy", synergy=False, ratio_var=1, mostrar_numeros=True, mostrar_elipse=False), 
                         use_container_width=True,
                         config={"staticPlot": True}
                         )
