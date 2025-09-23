05f8a603be0e281634989add6dc4ccc17e738673
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-23 19:50:14 -0300

    Actualizar todo

--- app_v1.py
+++ app_v1.py
@@ -52,7 +52,7 @@ pag_opencap = "Aplicando VFG: OpenCap"
 #st.sidebar.markdown("Selecciona una sección:")  # línea horizontal para separar
 
 # Sidebar 1
-pagina = st.sidebar.multiselect("Selecciona una sección:", [
+pagina = st.sidebar.checkbox("Selecciona una sección:", [
     pag_bienvenida,
     pag_introduccion,
     pag_bioinstrumentacion,
