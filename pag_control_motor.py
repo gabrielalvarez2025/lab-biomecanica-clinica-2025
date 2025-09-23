22ed0a0fa64357dc25fc41280a6f2e43d913ef81
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-23 19:26:26 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -475,11 +475,11 @@ def main_control_motor():
     lorem = (
     """
     <div style="color: white;">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
         <p>Otro párrafo en blanco</p>
     </div>
     """
-    * 5
+    * 50
     )
     
     st.markdown("---")
