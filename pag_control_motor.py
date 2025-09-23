67b42af2ed31a412f274aa7012611fc900fb6988
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-23 20:27:58 -0300

    Actualizar todo

--- pag_control_motor.py
+++ pag_control_motor.py
@@ -10,7 +10,7 @@ from pydub import AudioSegment
 def timeline_teorias_control_motor():
     
     tab_intro, tab_timeline = st.tabs([
-        "Haz click aquí ⮕",
+        "Haz click aquí  ⮕",
         "Línea de tiempo"
     ])
 
