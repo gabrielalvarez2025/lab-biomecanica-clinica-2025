70d4db9c3ed4adfacb50c18cc756656d10263b69
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-23 21:54:39 -0300

    Actualizar todo

--- pag_balance.py
+++ pag_balance.py
@@ -8,4 +8,22 @@ def main_balance():
     st.set_page_config(layout="centered", initial_sidebar_state="expanded")
     st.title("Balance y control postural")
 
+    seccion_intro = "Introducción al sistema sensoriomotor"
+    seccion_estabilidad = "Concepto de estabilidad"
+    seccion_propiocepcion = "La propiocepción"
+    seccion_ev_clinica = "Evaluación clínica del balance"
+    seccion_ev_instrumentada = "Evaluación instrumentada del balance"
+    
+    
+    sub_seccion = st.sidebar.radio("¿Qué te gustaría aprender?", [
+        seccion_intro,
+        seccion_estabilidad,
+        seccion_propiocepcion,
+        seccion_ev_clinica,
+        seccion_ev_instrumentada
+    ])
+
+    #if sub_seccion == seccion_intro:
+
+
     
