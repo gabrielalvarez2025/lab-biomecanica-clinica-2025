cf87360bf370c07e9322efc0809cd0d747b7b4ac
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-23 22:16:12 -0300

    Actualizar todo

--- pag_balance.py
+++ pag_balance.py
@@ -46,3 +46,13 @@ def main_balance():
             <button style="padding:6px 12px; font-size:14px;">📄 Abrir artículo PDF</button>
         </a>
         ''', unsafe_allow_html=True)
+    
+    st.subheader("Propiocepción")
+
+    st.markdown(
+        """
+        Proprioception predominates as the most misused term within the sensorimotor system. It has been incorrectly used synonymously and interchangeably with kinesthesia, joint position sense, somatosensation, balance, and reflexive joint stability. In Sherrington's13 original description of the “proprioceptive system,” proprioception was used to reference the afferent information arising from “proprioceptors” located in the “proprioceptive field.” The “proprioceptive field” was specifically defined as that area of the body “screened from the environment” by the surface cells, which contained receptors specially adapted for the changes occurring inside the organism independent of the “interoceptive field” (alimentary canal and viscera organs).13 In several of his writings, Sherrington13,14 declared proprioception as being used for the regulation of total posture (postural equilibrium) and segmental posture (joint stability), as well as initiating several conscious peripheral sensations (“muscle senses”). Although he considered vestibular information to be proprioceptive with respect to the head, Sherrington13 clearly delineated the functions of labyrinth from those receptors in the periphery. According to Matthews,15 Sherrington described 4 submodalities of “muscle sense” in Schafer's Textbook of Physiology: (1) posture, (2) passive movement, (3) active movement, and (4) resistance to movement. These submodality sensations correspond to the contemporary terms joint position sense (posture of segment), kinesthesia (active and passive), and the sense of resistance or heaviness. Thus, proprioception correctly describes afferent information arising from internal peripheral areas of the body that contribute to postural control, joint stability, and several conscious sensations.
+
+        In contrast to proprioception, the term somatosensory (or somatosensation) is more global and encompasses all of the mechanoreceptive, thermoreceptive, and pain information arising from the periphery.2 Conscious appreciation of somatosensory information leads to the sensations of pain, temperature, tactile (ie, touch, pressure, etc), and the conscious submodality proprioception sensations. Thus, as Figure 2 illustrates, conscious appreciation of proprioception is a subcomponent of somatosensation and, therefore, the terms should not be used interchangeably.
+        """
+    )
