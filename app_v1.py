a60c868b36868655ef90c5650a1385badc9daf53
Gabriel Álvarez Martínez <gabrielalvarezm.@MacBook-Pro-de-Gabriel.local>
2025-09-23 21:34:15 -0300

    Actualizar todo

--- app_v1.py
+++ app_v1.py
@@ -43,16 +43,20 @@ pag_marcha              = "Análisis de marcha"
 # Paginas adicionales
 pag_opencap = "Aplicando VFG: OpenCap"
 
-selected = option_menu(None, ["Home", "Upload", "Tasks", "QA", "Overall summary","Settings"], 
-        icons=['house', 'cloud-upload', "list-task", 'question-diamond-fill' , 'card-list' ,'gear'], 
-        menu_icon="cast", default_index=0, orientation="horizontal",
-        styles={
-                "container": {"padding": "0!important", "background-color": "#fafafa"},
-                "icon": {"color": "orange", "font-size": "25px"}, 
-                "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-                "nav-link-selected": {"background-color": "green"}
-            }
-        )
+selected = option_menu(
+    None, 
+    ["Home", "Upload", "Tasks", "QA", "Overall summary", "Settings"], 
+    icons=['house', 'cloud-upload', "list-task", 'question-diamond-fill', 'card-list', 'gear'], 
+    menu_icon="cast", 
+    default_index=0, 
+    orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "20px", "text-align": "center", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
 
 
 
